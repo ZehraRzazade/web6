f14462759c6cca4fbc1236b7dbca87417c70116c
RzazadeZehra <rzazadezehra02@gmail.com>
2022-05-10 10:23:53 +0400

    7thtask

--- src/components/task3/UserLogin.js
+++ src/components/task3/UserLogin.js
@@ -1,46 +1,67 @@
+
 import React, { Component } from 'react';
-// import ReactDOM from "react-dom";
-// import  PropTypes  from 'react';
+
 class Login extends Component {
+
     state = {
-        email: "",
-        password: ""
+        name: "",
+        location: "",
+        phone: "",
+        isValid: true,
+        index:0
     }
+    
+    handleSubmit = e => {
+        e.preventDefault();
+    };
     inputChange = (event) => {
+        if (event.target.name === "name") {
        
-        if (event.target.type == "text") {
+      
             this.setState({
-                email: event.target.value
+                index:this.state.index+1,
+                name: event.target.value
+                 
+})
+        } else if (event.target.name === "location") {
+         
+            this.setState({
+                index:this.state.index+1,
+                phone: event.target.value,
+               
 
             })
-        } else {
+        } else if (event.target.name === "phone") {
+        
+             this.setState({
+                index:this.state.index+1,
+                phone: event.target.value,
+                 })
+        }
+        
+        if(this.state.index===3){
             this.setState({
-                password: event.target.value
+                isValid: false
             })
         }
     }
-
-    handleSubmit = (event) => {
-        if (this.state.password.length < 8) {
-            alert("zehmet olmasa duzgun password daxil edin")
-        } else if (!this.state.email.endsWith(".ru")) {
-            alert("zehmet olmasa duzgun email yazin")
-        } else {
-            alert("form succesfully submit")
-        }
-    }
-
+    
     render() {
         return (
             <>
-                <form>
-                    <input type="text" onChange={this.inputChange} placeholder="email daxil edin" />
-                    <input type="password" onChange={this.inputChange} placeholder="parolunuzu daxil edin" />
-                    <button type='button' onClick={this.handleSubmit}>Login</button>
+                <form onSubmit={this.handleSubmit}>
+
+                    <input type="text" name="name" onChange={this.inputChange} placeholder="Ad soyadinizi daxil edin" />
+                    <input type="text" name="location" required onChange={this.inputChange} placeholder="Unvani daxil edin" />
+                    <input type="number" name="phone" onChange={this.inputChange} placeholder="Nomrenizi daxil edin" />
+                    <button type="submit" disabled={this.state.isValid}> Submit</button>
                 </form>
 
             </>
         )
     }
 }
+
+
+
 export default Login;
